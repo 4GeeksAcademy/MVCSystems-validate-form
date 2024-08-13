7199a3bb991d54dc0052579858ed02d28830eb00
MVCSystems <carlosmerav@gmail.com>
2024-08-13 16:27:46 -0500

    Add functions

--- src/app.js
+++ src/app.js
@@ -1,18 +1,5 @@
 // Description: Main javascript file for the application.
 
-// window.onload = function() {
-//   const dialog = document.querySelector("#error-msg");
-//   const form = document.querySelector("#sendForm");
-//   const fields = document.querySelectorAll("input,textarea,select");
-//   dialog.classList.add("d-none");
-//   form.addEventListener("click", function(e) {
-//     dialog.classList.remove("d-none");
-//     fields.forEach(function(field) {
-//       field.style.backgroundColor = "#f8d7da";
-//     });
-//   });
-// };
-
 let btn = document.getElementById("sendForm");
 btn.addEventListener("click", validarFormulario);
 
